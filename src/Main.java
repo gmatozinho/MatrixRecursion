96f865cc5407ca5c09a935e22a8afcab84bbc613
Gustavo <gustavomatozinho@gmail.com>
2018-03-18 22:58:38 -0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //E ele passou um exercício sobre matrizes.
+        // Elaborar uma função recursiva que retorne uma lista de itens da diagonal principal de uma matriz quadrada.
+        // Elaborar uma função recursiva que retorne uma lista de itens de uma matriz, caminhando em espiral pela matriz.
+
+
+        int[][] matrix = MatrixRecursion.CreateMat(3,6);
+        ArrayList<Integer> list = MatrixRecursion.MatrixDiag(matrix);
+
+        ArrayList<Integer> list2 = MatrixRecursion.MatrixEspiral(matrix);
+        System.out.println(list2);
     }
 }
---
+++ src/MatrixRecursion.java
@@ -0,0 +1,83 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class MatrixRecursion {
+
+
+    public static int[][] CreateMat(int rows, int cols)
+    {
+        int[][] mat = new int[rows][cols];
+        int number = 1;
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<cols;j++){
+                mat[i][j] = number ++;
+            }
+        }
+
+        return mat;
+    }
+
+
+    public static ArrayList<Integer> MatrixDiag(int[][] matrix)
+    {
+        return MatrixDiagTail(matrix, new ArrayList<>(),0);
+    }
+
+     /*@Matrix = Matriz que deseja percorrer
+      @List = Lista de retorno onde os elementos serão adicionados
+      @pos = inteiro com a posição diagonal
+      */
+    private static  ArrayList<Integer> MatrixDiagTail(int[][] matrix, ArrayList<Integer> array, int pos)
+    {
+        if(pos > matrix.length-1)
+        {
+            return array;
+        }
+        array.add(matrix[pos][pos]);
+        return MatrixDiagTail(matrix,array,pos+1);
+    }
+
+    public static ArrayList<Integer> MatrixEspiral(int[][] matrix){
+
+        int[] line = {0, matrix.length-1};
+        int[] column = {0, matrix[0].length-1};
+        return MatrixEspiralTail(matrix, new ArrayList<>(), line,column);
+
+    }
+
+    /*@Matrix = Matriz que deseja percorrer
+      @List = Lista de retorno onde os elementos serão adicionados
+      @row = array tamanho 2 com posição inicial e posição final para linhas
+      @col = array tamanho 2 com posição posição e posição final para coluna
+      Função para recursão em cauda
+      */
+    private static ArrayList<Integer> MatrixEspiralTail(int[][] matrix, ArrayList<Integer> list, int[] row, int[] col){
+        for (int i = row[0]; i <= col[1]; i++) {
+            list.add(matrix[row[0]][i]);
+        }
+
+        for (int i = row[0]+1; i <= row[1]; i++) {
+            list.add(matrix[i][col[1]]);
+        }
+
+        if(row[0]+1 <= row[1]){
+            for (int i = col[1]-1; i >= col[0]; i--) {
+                 list.add(matrix[row[1]][i]);
+            }
+        }
+
+        if(col[0]+1 <= col[1]){
+            for (int i = row[1]-1; i > row[0]; i--) {
+                list.add(matrix[i][col[0]]);
+            }
+        }
+
+        if(row[0]+1 <= row[1]-1 && col[0]+1 <= col[1]-1){
+            return MatrixEspiralTail(matrix,list,new int[]{row[0]+1, row[1]-1},new int[]{col[0]+1, col[1]-1});
+        }
+
+        return list;
+    }
+}
